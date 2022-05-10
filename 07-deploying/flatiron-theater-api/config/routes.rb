4dbe8e68d665d5080c00dd5db66fa04d705f34c1
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-05-10 19:35:28 -0400

    add solution for 07-deploying

--- 07-deploying/flatiron-theater-api/config/routes.rb
+++ 07-deploying/flatiron-theater-api/config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # Login / Logout Routes
   post '/login', to: 'sessions#login'
   delete '/logout', to: 'sessions#logout'
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
